e815de21bb99465bc6c8dfab828ce707c5b8b3d0
Tanim <88611927+from-tanim@users.noreply.github.com>
2024-10-17 05:32:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
 
-   btn.value = 'Sending...';
+    btn.value = 'Sending...';
 
-   const serviceID = 'service_aicy44q';
-   const templateID = 'template_71hqiv8';
+    const serviceID = 'service_aicy44q';
+    const templateID = 'template_71hqiv8';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
